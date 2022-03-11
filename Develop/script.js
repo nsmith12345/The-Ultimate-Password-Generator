203a3a080be0184c2dbcf581a257caa372bd9534
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-11 17:14:03 -0500

    return function and psuedocode added

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey! You clicked the button!")
+
+// 1. Prompt the user for the password criteria.
+//    a. Password Length 8 < 128.
+//    b. Lowercase, uppercase, numbers, special characters.
+// 2. Validate the input.
+// 3. Generate password based on criteria. 
+
+
+// 4. Display password to the page. 
+  return "Generated password will go here!"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
